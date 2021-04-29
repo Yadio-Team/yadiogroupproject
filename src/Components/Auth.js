c624dfa1ee1aa3b2ca1ec1dcc6c09b76d5f33b5b
Gabe Chavez <gabechavez714@gmail.com>
2021-04-29 09:06:03 -0700

    headerjs

--- src/Components/Auth.js
+++ src/Components/Auth.js
@@ -5,67 +5,65 @@ import { getUser } from '../redux/userReducer'
 import axios from 'axios';
 
 
-
-
 const Auth = ({ getUser }) => {
-    const [state, setState] = useState({
-      userName: '',
-      password: ''
-    })
-  
-    const history = useHistory()
- 
+  const [state, setState] = useState({
+    userName: '',
+    password: ''
+  })
 
-    const loginUser = async (e) => {
-        e.preventDefault()
-        const { userName, password } = state
-    
-        try {
-          const user = await axios.post('/auth/login', { userName, password })
-          getUser(user.data)
-          history.push('/home')
-        } catch (err) {
-          console.log(err)
-        }
-      }
-      const registerUser = async (e) => {
-        e.preventDefault()
-        const { userName, password } = state
-    
-        try {
-          const user = await axios.post('/auth/register', { userName, password })
-          getUser(user.data)
-          history.push('/home')
-        } catch (err) {
-          console.log(err)
-        }
-      }
+  const history = useHistory()
 
-      const changeHandler = e => setState({
-          ...state,
-          [e.target.name] : e.target.value
-      })
-      return (
-        <div className='auth'>
-          <h1 className='auth-title'>Sign In</h1>
-          <div className='auth-info' >
-            <input
-              className='auth-userName'
-              placeholder='User Name'
-              name='userName'
-              onChange={(e) => changeHandler(e)}
-            />
-            <input
-              className='auth-password'
-              placeholder='Password'
-              name='password'
-              onChange={(e) => changeHandler(e)}
-            />
-           <button className='sign-in-button' type='submit' onClick={loginUser}>Sign in</button>
-           <button className='register-button' type='submit' onClick={registerUser}>Register</button>
-          </div>
-        </div>
-      ) 
+
+  const loginUser = async (e) => {
+    e.preventDefault()
+    const { userName, password } = state
+
+    try {
+      const user = await axios.post('/auth/login', { userName, password })
+      getUser(user.data)
+      history.push('/home')
+    } catch (err) {
+      console.log(err)
+    }
+  }
+  const registerUser = async (e) => {
+    e.preventDefault()
+    const { userName, password } = state
+
+    try {
+      const user = await axios.post('/auth/register', { userName, password })
+      getUser(user.data)
+      history.push('/home')
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  const changeHandler = e => setState({
+    ...state,
+    [e.target.name]: e.target.value
+  })
+  return (
+    <div className='auth'>
+      <h1 className='auth-title'>Sign In</h1>
+      <div className='auth-info' >
+        <input
+          className='auth-userName'
+          placeholder='User Name'
+          name='userName'
+          onChange={(e) => changeHandler(e)}
+        />
+        <input
+          className='auth-password'
+          placeholder='Password'
+          name='password'
+          onChange={(e) => changeHandler(e)}
+        />
+        <button className='sign-in-button' type='submit' onClick={loginUser}>Sign in</button>
+        <button className='register-button' type='submit' onClick={registerUser}>Register</button>
+      </div>
+    </div>
+  )
 
 }
 
